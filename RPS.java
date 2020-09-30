d9ec75a1c0cdf05bd9d86fc3df2aab77095ac878
bfalsetto <benjamin.falsetto@ocsbstudent.ca>
2020-09-30 18:13:39 +0000

    Created file, but did not code

---
+++ RPS.java
@@ -0,0 +1,12 @@
+/**
+* This program plays rock paper scissors with the user.
+*
+* @author  Ben Falsetto
+* @version 1.0
+* @since   2020-09-30
+*/
+public class RPS {
+  public static void main(String[] args) {
+    
+  }
+}
